1b6e772aa63f769c639db9cf55916a180edd37a1
Blondberg <iamblondberg@gmail.com>
2021-04-07 20:16:41 +0200

    Basic project setup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # discord-bobot
+
 Bob is God.
+
+## Activate venv
+
+windows: .env/Scripts/activate
+linux: source .env/Scripts/activate
+
+## Install packages/modules
+
+pip freeze > requirements.txt
+pip install -r requirements.txt
---
+++ bobot/__main__.py
@@ -0,0 +1,23 @@
+import discord
+
+# Reads token from file.
+# DO NOT SHARE PUBLICLY!!!
+TOKEN = open('token.txt', 'r').read()
+
+client = discord.Client()
+
+@client.event
+async def on_ready(): 
+    print('We have logged in as {0.user}'.format(client))
+    
+@client.event
+async def on_message(message): 
+    if message.author == client.user: 
+        return 
+    
+    if message.content.startswith('$hello'): 
+        await message.channel.send('Hello!')
+
+
+client.run(TOKEN)      
+    
