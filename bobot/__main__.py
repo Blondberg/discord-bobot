1fde4d17cd5b63e738240f9b893595d0d43ecd74
Blondberg <iamblondberg@gmail.com>
2021-04-16 23:24:26 +0200

    Removed everything connected to lyrics, cause groovy is superior in every way

--- bobot/__main__.py
+++ bobot/__main__.py
@@ -10,7 +10,6 @@ from .cogs import leagueoflegends as lol
 from .cogs import voice
 from .core import messageformatter as mf
 from .cogs import random
-import lyricsgenius
 
 load_dotenv()
 
@@ -18,7 +17,6 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 PREFIX = os.getenv("BOT_PREFIX")
 CHANNEL_NAME = os.getenv("CHANNEL_NAME")
 GENIUS_TOKEN = os.getenv("GENIUS_TOKEN")
-genius = lyricsgenius.Genius(GENIUS_TOKEN)
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(PREFIX))
 
@@ -28,42 +26,8 @@ async def on_ready():
     print("I am ready!")
 
 
-@bot.event
-async def on_message(message):
-    if len(message.embeds) > 0:
-        if "now playing" in message.embeds[0].title.lower():
-            channel = discord.utils.find(
-                lambda c: "lyrics-by-bobot" in c.name, message.guild.channels
-            )
-            lyrics = chunkstring(check_lyrics(message.embeds[0].description))
-
-            for l in lyrics:
-                await channel.send(l)
-
-    await bot.process_commands(message)
-
-
 def chunkstring(string, length):
     return (string[0 + i : length + i] for i in range(0, len(string), length))
-
-
-def check_lyrics(desc):
-    try:
-        song_meta = desc[1 : desc.find("](")]
-        song_meta_list = song_meta.split(" - ")
-        song_artist = song_meta_list[0]
-        song_name = song_meta_list[1]
-
-        song = genius.search_song(song_name, song_artist)
-
-        if song.lyrics and len(song.lyrics) <= 5000:
-            return mf.format(song_meta) + "\n" + song.lyrics + "\n"
-        else:
-            raise exception
-    except:
-        return mf.format(
-            "Error: Could not find any lyrics for song: {0}".format(song_meta)
-        )
 
 
 voice.setup(bot)
@@ -71,7 +35,6 @@ voice.setup(bot)
 random.setup(bot)
 
 lol.setup(bot)
-
 
 bot.run(TOKEN)
 
--- bobot/cogs/music.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord.ext import commands
-
-
-class Music(commands.Cog, name="Music"):
-    def __init__(self, bot):
-        self.bot = bot
